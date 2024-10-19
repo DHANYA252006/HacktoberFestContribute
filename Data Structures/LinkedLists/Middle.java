fdad47c3e5cd046766ceccfe88725914ebb81310
DHANYA V <dhanyavellingiri2502@gmail.com>
2024-10-19 09:24:27 +0530

    Update Middle.java

--- Data Structures/LinkedLists/Middle.java
+++ Data Structures/LinkedLists/Middle.java
@@ -1,61 +1,72 @@
+//modified code
 import java.util.*;
 
 class Node {
-	int data;
-	Node next;
-	public Node() {
-		this.data = data;
-		this.next = null;
-	}
-	public Node(int d) {
-		this.data = d;
-	}
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
 }
 
 class Linked {
-	Node head;
-	Linked() {
-		head = null;
-	}
+    Node head;
 
-	void insertAtEnd(int data) {
-		Node n = new Node(data);
-		if(head == null) {
-			head = n;
-		} else {
-				Node temp;
-				for(temp = head; temp.next != null; temp = temp.next);
-				temp.next = n;
-				n.next = null;
-		}
-	}
-	void middleElement() {
-		Node temp;
-		int l = 1;
-		for(temp = head; temp.next != null; temp = temp.next) {
-			l++;
-		}
-		int pos = l/2;
-		int i;
-		for(temp = head, i = 0; temp.next.next != null && i <= pos - 1;temp = temp.next, i++);
-		System.out.println(temp.data);
-	}
+    Linked() {
+        head = null;
+    }
+
+    void insertAtEnd(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
+    void middleElement() {
+        if (head == null) {
+            System.out.println("The list is empty.");
+            return;
+        }
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        System.out.println("The middle element is: " + slow.data);
+    }
 }
 
-class Middle {
+public class Middle {
     public static void main(String[] args) {
-		Scanner Sc = new Scanner(System.in);
-		Linked ll = new Linked();
-        int size;
-        System.out.println("Enter the size:");
-        size = Sc.nextInt();
-        for( int s = 0; s < size; s++) {
-            System.out.println("Enter the data");
-            int data = Sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        Linked ll = new Linked();
+        
+        System.out.println("Enter the number of elements in the linked list:");
+        int size = sc.nextInt();
+
+        for (int i = 0; i < size; i++) {
+            System.out.println("Enter element " + (i + 1) + ":");
+            int data = sc.nextInt();
             ll.insertAtEnd(data);
         }
-        System.out.print("Element in the middle of the linked list is: ");
-		ll.middleElement();
+
+        ll.middleElement();
+        
+        // Close the scanner
+        sc.close();
     }
-}    
+}
 
