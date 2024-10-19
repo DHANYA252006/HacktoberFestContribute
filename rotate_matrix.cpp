b6b0e1405b6ae9fc8a4da9909a77ebfe770fc481
DHANYA V <dhanyavellingiri2502@gmail.com>
2024-10-19 09:02:46 +0530

    Create rotate_matrix.cpp

---
+++ rotate_matrix.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void rotateMatrixLayerByLayer(vector<vector<int>>& matrix) {
+    int n = matrix.size();
+
+    for (int layer = 0; layer < n / 2; layer++) {
+        int first = layer;
+        int last = n - 1 - layer;
+
+        for (int i = first; i < last; i++) {
+            int offset = i - first;
+            int top = matrix[first][i];
+            matrix[first][i] = matrix[last - offset][first];
+            matrix[last - offset][first] = matrix[last][last - offset];
+            matrix[last][last - offset] = matrix[i][last];
+            matrix[i][last] = top;
+        }
+    }
+}
